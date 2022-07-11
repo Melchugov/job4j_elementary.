9d6a8583a95192008f4dbc824bc1080a6f8d06de
evgen <evgenii_melchugov@mail.ru>
2022-07-11 22:26:08 +0700

    Git

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+
+        System.out.println("Melchugov Evgenii");
+        System.out.println("20.08.1997");
+    }
+}
