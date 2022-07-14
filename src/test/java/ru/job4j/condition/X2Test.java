906f704267157d8a20c6b900c635916dcc317249
evgen <evgenii_melchugov@mail.ru>
2022-07-14 19:03:13 +0700

    1.2. Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -18,7 +18,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenAminus1B1C1X1Then1 (){
+    public void whenAminus1B1C1X1Then1() {
         int a = -1;
         int b = 1;
         int c = 1;
@@ -30,7 +30,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenA0B1C1X1Then2 () {
+    public void whenA0B1C1X1Then2() {
         int a = 0;
         int b = 1;
         int c = 1;
@@ -41,7 +41,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenAminus1B1C0X1Then0 () {
+    public void whenAminus1B1C0X1Then0() {
         int a = -1;
         int b = 1;
         int c = 0;
@@ -52,7 +52,7 @@ public class X2Test {
     }
 
     @Test
-    public void whenAminus1B1C1X0Then1 () {
+    public void whenAminus1B1C1X0Then1() {
         int a = -1;
         int b = 1;
         int c = 1;
